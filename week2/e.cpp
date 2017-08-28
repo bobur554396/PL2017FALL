4f1ee92660ea986166cb5282cf95761c4ecd76f2
bobur554395 <bobur.muhsimbaev@gmail.com>
2017-08-28 16:40:41 +0600

    second lesson

--- week2/e.cpp
+++ week2/e.cpp
@@ -7,11 +7,13 @@ int main(){
 	int a = 2;
 	int b = 3;
 
+	cout << "a = " << a << endl << "b = " << b << endl;
 	cout << "a AND b = " << (a & b) << endl; 
 	cout << "a OR b = " << (a | b) << endl; 
 	cout << "a XOR b = " << (a ^ b) << endl; 
-	cout << "Not a = " << (~a) << endl; 
-
+	cout << "a Shift Left by b = " << (a << b) << endl; 
+	cout << "a Shift Right by b = " << (a >> b) << endl; 
+	cout << "Complement a = " << (~a) << endl; 
 
 	return 0;
 }
---
+++ week2/g.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+	int a = 2;
+	int b = 3;
+
+	cout << "a = " << a << endl << "b = " << b << endl;
+	cout << "(a == b) will be = " << (a == b) << "\n";
+	cout << "(a != b) will be = " << (a != b) << "\n";
+	cout << "(a >= b) will be = " << (a >= b) << "\n";
+	cout << "(a < b) will be = " << (a < b) << "\n";
+
+
+}
